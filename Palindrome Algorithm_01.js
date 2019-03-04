ab0330d9557575615dc9306d056f4521111d3198
major <echolabstech@gmail.com>
2019-03-04 18:46:26 +0800

    introduce breaking change

--- Palindrome Algorithm_01.js
+++ Palindrome Algorithm_01.js
@@ -4,7 +4,6 @@ function checkPalindrome(a){ //check if the String a is a palindrome. returns st
 	if(b.length % 2 == 0) {// if even number of letters
 		for(var n = 0; n < b.length/2; n++) {
 			if (b[n] != b[b.length - n - 1]) {
-				return 'false';
 			}
 		}
 	} else{// if odd number of letters
