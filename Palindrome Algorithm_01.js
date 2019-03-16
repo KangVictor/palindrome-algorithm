ae9098edfa234370f338ae3f446c5e62754e9239
Major Sapp III <echolabstech@gmail.com>
2019-03-16 11:50:27 +0800

    continuous input; allow exit on 'q' or 'Q' (#2)
    * introduce breaking change
    
    * Revert "introduce breaking change"
    
    This reverts commit 3e6c8f084fbd9bb7216ab5d3c1d2a97074d3daa5.
    
    * git pull rebas master
    
    * Changed the retun type of the function isPalindrome from string to Boolean

--- Palindrome Algorithm_01.js
+++ Palindrome Algorithm_01.js
@@ -1,36 +1,34 @@
-function checkPalindrome(a){ //check if the String a is a palindrome. returns string test "true" if so.
+function isPalindrome(a){ //check if the String a is a palindrome. returns string test "true" if so.
 	var b = a.split('');// split the string into array of char
 
 	if(b.length % 2 == 0) {// if even number of letters
 		for(var n = 0; n < b.length/2; n++) {
 			if (b[n] != b[b.length - n - 1]) {
-				return 'false';
+				return false;
 			}
 		}
 	} else{// if odd number of letters
 		for(var n = 0; n < (b.length + 1) / 2; n++) {
 			if (b[n] != b[b.length - n - 1]) {
-				return 'false';
+				return false;
 			}
 		}
 	}
 
-	return 'true';
+	return true;
 	
 }
-//var input = [];
 
 var readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
+console.log("input text: ");
+rl.on('line', (line) => {
+if (line.toLowerCase() == 'q') rl.close();
+  if (isPalindrome(line) === true) {
+  	console.log(`"${line}" is a palindrome :-)`)
+  } else {
+  	console.log(`"${line}" is not a palindrome :-(`)
+  }
 
-
-rl.question("input text: ", function(answer){
-	console.log(checkPalindrome(answer));
+	console.log("input text: ");
 });
-
-//}
-//checkPalindrome(answer);
-		//input[0] = checkPalindrome(answer);
-//for(var n = 0; n < 3; n++) {
-	//console.log(input[n]);
-//}
