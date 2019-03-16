d8e4bc03f751262257ee05db367cc0bef77222f8
KangVictor <orisho0330@gmail.com>
2019-03-16 11:47:12 +0800

    Changed the retun type of the function isPalindrome from string to Boolean

--- Palindrome Algorithm_01.js
+++ Palindrome Algorithm_01.js
@@ -4,18 +4,18 @@ function isPalindrome(a){ //check if the String a is a palindrome. returns strin
 	if(b.length % 2 == 0) {// if even number of letters
 		for(var n = 0; n < b.length/2; n++) {
 			if (b[n] != b[b.length - n - 1]) {
-				return 'false';
+				return false;
 			}
 		}
 	} else{// if odd number of letters
 		for(var n = 0; n < (b.length + 1) / 2; n++) {
 			if (b[n] != b[b.length - n - 1]) {
-				return 'false';
+				return false;
 			}
 		}
 	}
 
-	return 'true';
+	return true;
 	
 }
 
@@ -23,9 +23,8 @@ var readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
 console.log("input text: ");
 rl.on('line', (line) => {
-	if (line.toLowerCase() === 'q') rl.close();
-
-  if (isPalindrome(line)) {
+if (line.toLowerCase() == 'q') rl.close();
+  if (isPalindrome(line) === true) {
   	console.log(`"${line}" is a palindrome :-)`)
   } else {
   	console.log(`"${line}" is not a palindrome :-(`)
