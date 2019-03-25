cd00c34694250d446ee9969132dfb1ad338f22e6
major <echolabstech@gmail.com>
2019-03-25 18:13:33 +0800

    fix flask server method not methods

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/words', methods = ['POST'])
 def words():
-	if request.methods == 'POST':
+	if request.method == 'POST':
 		if request.form['words_input']:
 			return 'input successful'
 		else: 
