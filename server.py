7d3b3e3573dd99ab186bd1a6d0117f552e4650c7
KangVictor <orisho0330@gmail.com>
2019-04-02 14:58:45 +0800

    create words_data.txt file if doesnt exist. add palindrome words when submitted.

--- server.py
+++ server.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 @app.route('/words', methods = ['POST'])
 def words():
 	if request.is_json:
-		words_input = request.get_json()
-		print (words_input)
-		
-		
-	return '{"error": "expecting json"}'
+		getWord = request.get_json()
+		print (getWord['words_input'])
+		# write words_input on words_data.txt
+		f = open("words_data.txt","a+")
+		f.write("\n%s" % getWord['words_input'])
+		f.close()
+	return 'true'
 
 @app.route('/palindrome/')
 def palindrome():
