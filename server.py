5e1fc27a0bbfe314514a6d46e31f03eb6324b852
KangVictor <orisho0330@gmail.com>
2019-04-04 17:32:32 +0800

    same palindrome words that are already in the words_data.txt no longer goes in`

--- server.py
+++ server.py
@@ -7,10 +7,25 @@ def words():
 	if request.is_json:
 		getWord = request.get_json()
 		print (getWord['words_input'])
-		# write words_input on words_data.txt
-		f = open("words_data.txt","a+")
-		f.write("\n%s" % getWord['words_input'])
+		# checks if it there is already the word 
+		f = open("words_data.txt", "r")
+		read_line = ' '
+		check_exist = False
+		while read_line != '':
+			read_line = f.readline()
+			print read_line
+			if read_line == (getWord['words_input']):
+				check_exist = True
+			if read_line == (getWord['words_input'] + '\n'):
+				check_exist = True
 		f.close()
+
+		if check_exist != True:
+			# write words_input on words_data.txt
+			f = open("words_data.txt","a+")
+			f.write("\n%s" % getWord['words_input'])
+			f.close()
+
 	return 'true'
 
 @app.route('/palindrome/')
