c73c26694b1eacc248a6d786f91e66623cd84e0c
major <echolabstech@gmail.com>
2019-06-02 15:28:37 +0800

    test palindrome algorithm

---
+++ test_palindrome_algorithm.js
@@ -0,0 +1,7 @@
+const isPalindrome = require('./palindrome_algorithm.js');
+
+console.assert(isPalindrome('mom'));
+console.assert(!isPalindrome('healthcare'));
+// 'dad'
+// 'ecstatic'
+// 'racecar'
